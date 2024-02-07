857dda2626d8f7f5751305981fa3d362b1c0c57e
Olga Dimova <olgadimovaa@gmail.com>
2024-02-07 21:56:47 +0300

    Добавила страницы логин и регистрация, обновила vite конфигурацию

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: resolve(__dirname, 'src'),
   server: {
     port: 3000,
   },
@@ -10,4 +12,14 @@ export default defineConfig({
       context: {},
     }),
   ],
-});
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "/styles/style.css";
+          @import "/styles/variables.scss";
+        `,
+      },
+    },
+  },
+})
