93bb5a75c5162a8f0f1f4d1216e1e21082e232d1
Olga Dimova <olgadimovaa@gmail.com>
2024-02-10 18:02:20 +0300

    Обновила стили для всех страниц, добавила чаты и сообщения

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,84 @@ export default defineConfig({
   },
   plugins: [
     handlebars({
-      context: {},
+      context: {
+        selectedChatId: 1,
+        chats: [
+          {
+            id: 1,
+            title: 'Андрей',
+            avatar: '',
+            unreadMessages: 2,
+            lastMessage: {
+              title: 'Привет',
+              createdAt: '10:30',
+            },
+            messages: [
+              {
+                isMine: true,
+                title: 'Привет',
+                createdAt: '10:30',
+              },
+              {
+                isMine: false,
+                title: 'И тебе',
+                createdAt: '10:30',
+              },
+            ],
+          },
+          {
+            id: 2,
+            title: 'Киноклуб',
+            avatar: '',
+            unreadMessages: 0,
+            lastMessage: {
+              title: 'Вы: до завтра!',
+              createdAt: 'Пн',
+            },
+            messages: [
+              {
+                isMine: true,
+                title: 'до завтра!',
+                createdAt: 'Пн',
+              },
+            ],
+          },
+          {
+            id: 3,
+            title: 'Hi',
+            avatar: '',
+            unreadMessages: 4,
+            lastMessage: {
+              title: 'Как дела?',
+              createdAt: 'Пн',
+            },
+            messages: [
+              {
+                isMine: false,
+                title: 'Как дела?',
+                createdAt: 'Пн',
+              },
+            ],
+          },
+          {
+            id: 4,
+            title: 'Рандом',
+            avatar: '',
+            unreadMessages: 0,
+            lastMessage: {
+              title: 'тестовое сообщение',
+              createdAt: '10 мая 2020',
+            },
+            messages: [
+              {
+                isMine: true,
+                title: 'тестовое сообщение',
+                createdAt: '10 мая 2020',
+              },
+            ],
+          },
+        ],
+      },
     }),
   ],
   css: {
