d062d9ebca291702892242b3e2f1d42231d5bbd7
Olga Dimova <olgadimovaa@gmail.com>
2024-02-11 17:04:44 +0300

    Настроила сервер и start скрипт, исправила ошибки с иконками

---
+++ server.cjs
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/assets'));
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/register.html'));
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/chat.html'));
+});
+
+app.get('/password-update', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/password-update.html'));
+});
+
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/profile.html'));
+});
+
+app.get('/profile-update', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/password-update.html'));
+});
+
+app.get('/404', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/404.html'));
+});
+
+app.get('/500', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/500.html'));
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер слушает!`);
+});
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,22 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        register: resolve(__dirname, 'src/register.html'),
+        profile: resolve(__dirname, 'src/profile.html'),
+        profileUpdate: resolve(__dirname, 'src/profile-update.html'),
+        passwordUpdate: resolve(__dirname, 'src/password-update.html'),
+        chat: resolve(__dirname, 'src/chat.html'),
+        404: resolve(__dirname, 'src/404.html'),
+        500: resolve(__dirname, 'src/500.html'),
+      },
+    },
+  },
   server: {
     port: 3000,
   },
